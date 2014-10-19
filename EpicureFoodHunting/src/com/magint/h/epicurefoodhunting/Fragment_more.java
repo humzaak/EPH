b3cf35913d67e57ed12386665914a9c88cfe1dbb
humzaak <hakhtar@ntu.edu.sg>
2014-10-19 20:30:19 +0800

    fixed fragmentactivity issue in TutorialActivity

--- EpicureFoodHunting/src/com/magint/h/epicurefoodhunting/Fragment_more.java
+++ EpicureFoodHunting/src/com/magint/h/epicurefoodhunting/Fragment_more.java
@@ -1,10 +1,15 @@
 package com.magint.h.epicurefoodhunting;
 
+//import com.example.android.animationsdemo.MainActivity;
+
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 //import com.magint.h.epicurefoodhunting.MainActivity.Fragment_more;
 
@@ -35,11 +40,32 @@ private static final String ARG_SECTION_NUMBER = "section_number";
  public Fragment_more() {
     }       
 
+
  
  @Override
  public View onCreateView(LayoutInflater inflater, ViewGroup container,
          Bundle savedInstanceState) {
      View rootView = inflater.inflate(R.layout.fragment_more, container, false);
+     
+
+     final Button button = (Button) rootView.findViewById(R.id.tutorialbutton);
+     button.setOnClickListener(new View.OnClickListener() {
+         public void onClick(View v) {
+               startActivity(new Intent(getActivity(), TutorialActivity.class));
+               
+         }
+     });
+     
+     final Button button1 = (Button) rootView.findViewById(R.id.aboutbutton);
+     button1.setOnClickListener(new View.OnClickListener() {
+         public void onClick(View v) {
+        	 String url = getString(R.string.epicureurl);
+        	 Intent i = new Intent(Intent.ACTION_VIEW);
+        	 i.setData(Uri.parse(url));
+        	 startActivity(i);               
+         }
+     });
+     
      
      return rootView;
  }
