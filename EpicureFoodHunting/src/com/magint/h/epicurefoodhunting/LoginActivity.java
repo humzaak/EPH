c9eebf0d4abdb961387ce9708c60953d73de8970
humzaak <hakhtar@ntu.edu.sg>
2014-10-25 00:52:24 +0800

    facebook1

--- EpicureFoodHunting/src/com/magint/h/epicurefoodhunting/LoginActivity.java
+++ EpicureFoodHunting/src/com/magint/h/epicurefoodhunting/LoginActivity.java
@@ -25,7 +25,7 @@ public class LoginActivity extends Activity {
 
 	
 	//hello
-	
+	//hellow
 	 @Override
 	    protected void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
