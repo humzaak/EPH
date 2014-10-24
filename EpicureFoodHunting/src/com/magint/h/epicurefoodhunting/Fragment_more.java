12be7869164645dcb57a946de58be23abf446011
humzaak <hakhtar@ntu.edu.sg>
2014-10-25 00:42:00 +0800

    facebook

--- EpicureFoodHunting/src/com/magint/h/epicurefoodhunting/Fragment_more.java
+++ EpicureFoodHunting/src/com/magint/h/epicurefoodhunting/Fragment_more.java
@@ -2,14 +2,18 @@ package com.magint.h.epicurefoodhunting;
 
 //import com.example.android.animationsdemo.MainActivity;
 
+import com.facebook.Session;
+
 import android.app.Fragment;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 //import com.magint.h.epicurefoodhunting.MainActivity.Fragment_more;
 
@@ -24,6 +28,10 @@ public  class Fragment_more extends Fragment {
      * fragment.
      */
 private static final String ARG_SECTION_NUMBER = "section_number";
+	private static final int RESULT_OK = -1;
+	private static final int RESULT_CANCELED = 1;
+	private TextView userName;
+	private TextView welCome;
 
     /**
      * Returns a new instance of this fragment for the given section
@@ -47,6 +55,29 @@ private static final String ARG_SECTION_NUMBER = "section_number";
          Bundle savedInstanceState) {
      View rootView = inflater.inflate(R.layout.fragment_more, container, false);
      
+     Session session = Session.getActiveSession();
+     if (session != null) {
+
+         if (!session.isClosed()) {
+             session.closeAndClearTokenInformation();
+             Log.d("se","seesion is opened");
+             //clear your preferences if saved
+         }
+         else{
+        	 
+             Log.d("se","seesion is closed");
+
+         }
+     }
+     else{
+    	 
+         Log.d("se","seesion is null");
+
+    	 
+     }
+     
+     
+     
 
      final Button button = (Button) rootView.findViewById(R.id.tutorialbutton);
      button.setOnClickListener(new View.OnClickListener() {
@@ -55,6 +86,15 @@ private static final String ARG_SECTION_NUMBER = "section_number";
                
          }
      });
+     
+     final Button button2 = (Button) rootView.findViewById(R.id.loginbutton);
+     button2.setOnClickListener(new View.OnClickListener() {
+         public void onClick(View v) {
+               startActivityForResult(new Intent(getActivity(), LoginActivity.class),1);
+
+         }
+     });
+     
      
      final Button button1 = (Button) rootView.findViewById(R.id.aboutbutton);
      button1.setOnClickListener(new View.OnClickListener() {
@@ -67,6 +107,34 @@ private static final String ARG_SECTION_NUMBER = "section_number";
      });
      
      
+     
+     
+     
+     
      return rootView;
  }
+ 
+ public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+	    if (requestCode == 1) {
+	        if(resultCode == RESULT_OK){
+	            String result=data.getStringExtra("result");
+	            Log.d("lk",result);
+	    		userName = (TextView) getActivity().findViewById(R.id.usernametextView);
+				userName.setText(result);
+				
+				welCome = (TextView) getActivity().findViewById(R.id.welcometextView);
+				welCome.setText("Welcome");
+				
+			    final Button button_login = (Button) getView().findViewById(R.id.loginbutton);
+			    button_login.setText("Logout");
+
+	        }
+	        if (resultCode == RESULT_CANCELED) {
+	            //Write your code if there's no result
+	        }
+	    }
+	}
+ 
+ 
 }
---
+++ EpicureFoodHunting/src/com/magint/h/epicurefoodhunting/LoginActivity.java
@@ -0,0 +1,141 @@
+package com.magint.h.epicurefoodhunting;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.List;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.facebook.*;
+import com.facebook.model.*;
+import com.facebook.widget.LoginButton;
+
+public class LoginActivity extends Activity {
+
+	
+	
+	
+	 @Override
+	    protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.login_signup_dialog);
+
+	        final ActionBar actionBar = getActionBar();
+	        actionBar.setHomeButtonEnabled(true); 
+        
+	       LoginButton authButton = (LoginButton) findViewById(R.id.facebook_login_button);
+	        authButton.setReadPermissions(Arrays.asList("public_profile","email"));
+	        
+	        authButton.setUserInfoChangedCallback(new LoginButton.UserInfoChangedCallback() {
+	            @Override
+	            public void onUserInfoFetched(GraphUser user) {
+	                // It's possible that we were waiting for this.user to be populated in order to post a
+	                // status update.
+	            	updateUI(user);
+	            	if(user!=null)
+	            	{
+
+	            	}
+	            	else{
+	            		
+	            		Log.d("username1","you are not logged in");
+	            	}
+	            
+	            }
+	            
+	           
+	        });
+	        
+	      
+	        
+	        
+	        
+	     /*   
+
+	      Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+	          // callback when session changes state
+	         // @Override
+	          public void call(Session session, SessionState state, Exception exception) {
+	            
+	        	  if (session.isOpened()) {
+  
+	              // make request to the /me API
+	              Request.newMeRequest(session, new Request.GraphUserCallback() {
+	            	  
+	                // callback after Graph API response with user object
+	                @Override
+	                public void onCompleted(GraphUser user, Response response) {
+	                  if (user != null) {
+	                    //TextView welcome = (TextView) findViewById(R.id.welcome);
+	                    //welcome.setText("Hello " + user.getName() + "!");
+	                	 // Log.d("username",user.getFirstName());
+	                	 // Log.d("username",(String) user.asMap().get("email"));
+	                	 // Intent returnIntent = new Intent();
+	                	  //returnIntent.putExtra("result",user.getFirstName());
+	                	  //setResult(RESULT_OK,returnIntent);
+	                	 // finish();
+	                  }
+	                  else{
+		            		
+		            		Log.d("username2","you are not logged in");
+		            	}
+	                  
+	                }
+	              }).executeAsync();
+	            }
+	          }
+	        });
+	        */
+	        
+	    }
+
+	   
+	    @Override
+	    public boolean onOptionsItemSelected(MenuItem item) {
+	        switch (item.getItemId()) {
+	        case android.R.id.home:
+	            this.finish();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	        }
+	    }
+	 
+	   
+	    @Override
+	    public boolean onCreateOptionsMenu(Menu menu) {
+	        // Inflate the menu; this adds items to the action bar if it is present.
+	        getMenuInflater().inflate(R.menu.main, menu);
+	        return true;
+	    }
+	    
+	    @Override
+	    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	      super.onActivityResult(requestCode, resultCode, data);
+	      Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+	    }
+
+	    private void updateUI(GraphUser user){
+	    	 Session session = Session.getActiveSession();
+	    	 if(session != null && session.isOpened() && user != null){
+	    		 
+            	  Intent returnIntent = new Intent();
+           	      returnIntent.putExtra("result",user.getFirstName());
+           	      setResult(RESULT_OK,returnIntent);
+           	      finish();
+	    	 }
+	    	}
+	
+}
